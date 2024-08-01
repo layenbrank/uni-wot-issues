11858b2b6865f40959b5853fef89d90cc8787344
李贺 <15638470820@163.com>
2024-08-01 18:10:36 +0800

    docs(index): 首页 tabs issues
    wot design ui tabs issues

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,25 +8,21 @@
 }
 </route>
 <template>
-  <view
-    class="bg-white overflow-hidden pt-2 px-4"
-    :style="{ marginTop: safeAreaInsets?.top + 'px' }"
-  >
-    <view class="mt-12">
-      <image src="/static/logo.svg" alt="" class="w-28 h-28 block mx-auto" />
-    </view>
-    <view class="text-center text-4xl main-title-color mt-4">unibest</view>
-    <view class="text-center text-2xl mt-2 mb-8">最好用的 uniapp 开发模板</view>
-
-    <view class="text-justify max-w-100 m-auto text-4 indent mb-2">{{ description }}</view>
-    <view class="text-center mt-8">
-      当前平台是：
-      <text class="text-green-500">{{ PLATFORM.platform }}</text>
-    </view>
-    <view class="text-center mt-4">
-      模板分支是：
-      <text class="text-green-500">base</text>
-    </view>
+  <view class="container">
+    <wd-tabs v-model="tab">
+      <block v-for="item in 2" :key="item">
+        <wd-tab :title="`标签${item}`">
+          <view class="content">内容{{ item }}</view>
+          <wd-tabs v-model="tab1">
+            <block v-for="item in tabs" :key="item">
+              <wd-tab :title="`${item}`" :name="item">
+                <view class="content">内容 {{ item }}</view>
+              </wd-tab>
+            </block>
+          </wd-tabs>
+        </wd-tab>
+      </block>
+    </wd-tabs>
   </view>
 </template>
 
@@ -43,6 +39,11 @@ const author = ref('菲鸽')
 const description = ref(
   'unibest 是一个集成了多种工具和技术的 uniapp 开发模板，由 uniapp + Vue3 + Ts + Vite4 + UnoCss + UniUI + VSCode 构建，模板具有代码提示、自动格式化、统一配置、代码片段等功能，并内置了许多常用的基本组件和基本功能，让你编写 uniapp 拥有 best 体验。',
 )
+
+const tab = ref<number>(0)
+const tabs = ref(['这', '是', '一', '个', '例子'])
+const tab1 = ref('例子')
+
 // 测试 uni API 自动引入
 onLoad(() => {
   console.log(author)
@@ -52,5 +53,8 @@ onLoad(() => {
 <style>
 .main-title-color {
   color: #d14328;
+}
+.container {
+  @apply 'w-full h-100vh flex items-center justify-center';
 }
 </style>
